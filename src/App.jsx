1d12711837dd82cd58133df95a6e64fd9a2f6b98
Ajit <yajit1908@gmail.com>
2025-02-14 15:12:01 +0530

    React Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='Container bg-color-blue'>
+      <h1>Hello Everyone </h1>
+    </div>
+  )
+}
+
+export default App
